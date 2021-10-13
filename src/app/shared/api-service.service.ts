3469a819dd06859e673d9d604a14eb3218c183f8
babumiya <88158658+babumiya@users.noreply.github.com>
2021-10-13 18:34:30 +0530

    new file changed 13-10-2021

--- src/app/shared/api-service.service.ts
+++ src/app/shared/api-service.service.ts
@@ -14,4 +14,22 @@ export class ApiServiceService {
       return res;
     }))
   }
+  postData(data:any,url:any){
+    return this.http.get(url,data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  updateData(data:any,url:any,id:any){
+    return this.http.get(url+id,data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  deleteData(url:any,id:any){
+    return this.http.get(url+id)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
 }
